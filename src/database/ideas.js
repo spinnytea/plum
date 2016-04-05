f9ad49c735383340010501e39dd4c07f479198b3
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2016-04-04 23:31:38 -0400

    geting started with ideas
    forgot to make the load/save return promises; backing out

--- src/database/ideas.js
+++ src/database/ideas.js
@@ -1,3 +1,22 @@
 'use strict';
+// these are the vertices of the though graph
+// this is how all the data is stored
+const _ = require('lodash');
+
+const memory = new Map();
+
 
 Object.defineProperty(exports, 'units', { value: {} });
+exports.units.memory = memory;
+exports.units.getID = getID;
+
+function getID(idea) {
+  var id = idea.id || idea;
+  if(!_.isString(id)) throw new TypeError('can only load ideas');
+  return id;
+}
+
+
+Object.defineProperty(exports, 'boundaries', { value: {} });
+exports.boundaries.load = undefined;
+exports.boundaries.save = undefined;
--- unit/database/ideas.js
+++ unit/database/ideas.js
@@ -4,6 +4,42 @@ const ideas = require('../../src/database/ideas');
 
 describe('ideas', function() {
   it('init', function() {
-    expect(Object.keys(ideas.units)).to.deep.equal([]);
+    expect(Object.keys(ideas.units)).to.deep.equal(['memory', 'getID']);
+    expect(Object.keys(ideas.boundaries)).to.deep.equal(['load', 'save']);
   });
+
+  it('memory', function() {
+    // okay, I know, this is just testing maps
+    // but this is how memory is used
+    let map = new Map();
+    let key = 'thing1';
+    let value = 10;
+
+    expect(map.has(key)).to.equal(false);
+    map.set(key, value);
+    expect(map.has(key)).to.equal(true);
+    expect(map.get(key)).to.equal(value);
+    map.delete(key);
+    expect(map.has(key)).to.equal(false);
+
+    map.set(key, value);
+    expect(map.has(key)).to.equal(true);
+    map.clear();
+    expect(map.has(key)).to.equal(false);
+  });
+
+  it('getID', function() {
+    const getID = ideas.units.getID;
+
+    expect(getID('_test')).to.equal('_test');
+    expect(getID({ id: '_test' })).to.equal('_test');
+
+    expect(function() { getID(); }).to.throw(TypeError);
+    expect(function() { getID(1234); }).to.throw(TypeError);
+    expect(function() { getID({}); }).to.throw(TypeError);
+  });
+
+  it.skip('load');
+
+  it.skip('save');
 }); // end ideas
