7f6f2049bb3069af25e7fe49a579c3ab6746d4d2
Garrett Henderson-Tjarks <spinnytea@gmail.com>
2016-04-15 19:57:21 -0400

    planning out subgraph

--- src/database/subgraph.js
+++ src/database/subgraph.js
@@ -1,3 +1,37 @@
 'use strict';
 
+// this is an overlay on the idea database
+// it is a proxy or wrapper around the idea graph
+// it's main purpose is to find a subgraph within the larger database
+//
+// you define the shape the graph you want to find, each node has it's own matcher
+//
+// there are three different stages to this subgraph
+// each vertex contains data for these three stages
+// for the sake of efficiency, they are not stored together
+class Subgraph {
+  constructor() {
+    this._match = new LazyCopyObject();
+  }
+}
+
+class LazyCopyObject {
+  constructor() {
+    this.data = {};
+    this.parent = undefined;
+  }
+
+  get(id) {
+    if(id in this.data)
+      return this.data[id];
+
+    if(this.parent)
+      return this.parent.get(id);
+
+    return undefined;
+  }
+}
+
 Object.defineProperty(exports, 'units', { value: {} });
+exports.units.LazyCopyObject = LazyCopyObject;
+exports.units.Subgraph = Subgraph;
--- unit/database/subgraph.js
+++ unit/database/subgraph.js
@@ -4,6 +4,31 @@ const subgraph = require('../../src/database/subgraph');
 
 describe('subgraph', function() {
   it('init', function() {
-    expect(Object.keys(subgraph.units)).to.deep.equal([]);
+    expect(Object.keys(subgraph.units)).to.deep.equal(['LazyCopyObject', 'Subgraph']);
   });
+
+  describe('LazyCopyObject', function() {
+    describe('get local', function() {
+      it.skip('found');
+
+      it.skip('not found');
+    }); // end get local
+
+    describe('get parent', function() {
+      it.skip('found');
+
+      it.skip('not found');
+
+      it.skip('overwritten locally');
+    }); // end get parent
+  }); // end LazyCopyObject
+
+  describe('Subgraph', function() {
+    it('construct', function() {
+      var sg = new subgraph.units.Subgraph();
+      expect(sg).to.have.property('_match');
+    });
+
+    it.skip('subgraph');
+  }); // end Subgraph
 }); // end subgraph
